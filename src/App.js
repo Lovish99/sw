5df3f7849750711432cf048c45ea3addb69add71
Lovish Sethi <lovish.sethi@american-technology.net>
2023-03-27 21:39:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Lovish sethi
+          Lovish sethi 222
         </a>
       </header>
     </div>
